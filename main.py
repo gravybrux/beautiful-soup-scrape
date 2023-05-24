34f38549f075b837335128b87dd662010463b83e
xavbruggen <xavier.bruggen@northcoders.com>
2023-05-24 11:08:10 +0100

    feat: scrape 2

--- main.py
+++ main.py
@@ -2,32 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# URL of the page
 url = 'https://www.coursereport.com/tracks/full-stack-developer'
 
-# Send GET request to the page
 response = requests.get(url)
 
-# If the GET request is successful, the status code will be 200
 if response.status_code == 200:
     
-    # Get the content of the response
     page_content = response.content
     
-    # Create a BeautifulSoup object and specify the parser
     soup = BeautifulSoup(page_content, 'html.parser')
     
-    # Find all divs with 'data-ga' attribute equals 'card-title'
-    divs = soup.find_all('h3', attrs={'data-ga': 'card-title'})
+    list_items = soup.find_all('li', attrs={'data-ga': 'card'})
     
-    # Create a list to hold the data
     data = []
     
-    # Loop through the divs and get the text inside the div
-    for div in divs:
-        data.append({'name': div.text.strip()})
+    for li in list_items:
+        h3 = li.find('h3', attrs={'data-ga': 'card-title'})
+        
+        a_elements = li.find_all('a', href=lambda value: value and value.startswith("/tracks/"))
+        
+        tracks = [a.text.strip() for a in a_elements]
+
+        if h3:
+            data.append({'name': h3.text.strip(), 'tracks': tracks})
     
-    # Write data to a JSON file
     with open('data.json', 'w') as f:
         json.dump(data, f)
 else:
